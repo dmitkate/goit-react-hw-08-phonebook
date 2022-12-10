6ea750b8ebda4da531b0c1d8a9f55b54067fe8be
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-12-10 11:16:40 +0000

    Deploying to gh-pages from @ dmitkate/goit-react-hw-08-phonebook@782bc374ec9b9b40478a5f91c7e2f2ae8d165db6 🚀

---
+++ static/js/839.fd038b9b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[839],{5839:function(e,t,n){n.r(t);n(2791);var i=n(184),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};t.default=function(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsx)("h1",{style:c.title,children:"CREATE YOUR CONTACTS BOOK"})})}}}]);
+//# sourceMappingURL=839.fd038b9b.chunk.js.map
--- static/js/947.753001bc.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[947],{947:function(e,t,n){n.r(t),n.d(t,{NotFound:function(){return c}});var r=n(184),c=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that page :("})]})}}}]);
-//# sourceMappingURL=947.753001bc.chunk.js.map
