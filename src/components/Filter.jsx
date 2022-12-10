afa11d6a5c73ee580c71948580a27910a98785c9
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-12-10 10:30:47 +0200

    add design

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,16 +1,25 @@
 import { useDispatch } from 'react-redux';
 import { setNameFilter } from '../redux/contacts/filterslice';
+import { Input, Flex, Heading } from '@chakra-ui/react';
 function Find() {
   const dispatch = useDispatch();
-  //const filter = useSelector(state => state.filter);
+
   const onFilterChange = e => {
     dispatch(setNameFilter(e.currentTarget.value));
   };
   return (
-    <label>
-      Find contact by name
-      <input name="search" onChange={onFilterChange} />
-    </label>
+    <Flex flexDirection="column" alignItems="center">
+      <Heading textAlign="left" as="h2" size="26px" color="#1B3F34">
+        CONTACTS LIST:
+      </Heading>
+      <Input
+        maxW="550px"
+        variant="flushed"
+        placeholder="Find contact by name"
+        name="search"
+        onChange={onFilterChange}
+      />
+    </Flex>
   );
 }
 export default Find;
